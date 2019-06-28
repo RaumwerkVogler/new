824f81ae3c41bc0d08d2e919118c802ead4ef2cd
br-10 <kontakt@herr-broich.de>
2019-06-28 22:34:21 +0200

    auto

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import React from "react"
 import styled from 'styled-components'
 import { DatenProvider } from "./src/context/DatenContext"
-
+import './src/styles/global.css'
 export const wrapRootElement = ({ element }) => (
   <DatenProvider>{element}</DatenProvider>
 )
