70e3878e2a5e83326e1a6fffcba1a142be361c69
br-10 <kontakt@herr-broich.de>
2019-06-30 13:16:24 +0200

    provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,22 @@
 
 import React from "react"
 import styled from 'styled-components'
+import {MDXProvider} from '@mdx-js/react'
 import { DatenProvider } from "./src/context/DatenContext"
 import './src/styles/global.css'
-export const wrapRootElement = ({ element }) => (
-  <DatenProvider>{element}</DatenProvider>
+
+
+const components = {
+ h2: props =>(
+  <h2 style={{color:'hotpink',fontSize:'1.2rem'}} {...props} />
 )
+}
+export const wrapRootElement = ({ element }) => {
+return (
+
+  <DatenProvider>
+<MDXProvider components={components}>{element}</MDXProvider>
+  </DatenProvider>
+
+
+)}
